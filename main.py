439d764199c3b6713d773e7e006ed3ac8121ec60
ZatX <zatxmapki@gmail.com>
2020-04-22 16:15:58 +0200

    class Player added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#yeet
+class Player:
+    def __init__(self, name, health, mana, strength, agility):
+        self.name = name
+        self.hp = health
+        self.mn = mana
+        self.str = strength
+        self.agl = agility
+
+    def stats(self):
+        print("Name: ", self.name)
+        print("HP", self.hp, "MN", self.mn, "STR", self.str, "AGL", self.agl)
+
+player = Player("Player1", 100, 50, 10, 10)
+
+player.stats()
